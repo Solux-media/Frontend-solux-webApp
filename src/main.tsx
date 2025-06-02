3ff4953a64efce70c046110c22bfb5cbe597f208
Brunnotte Wasaulua <bt.wasaulua@gmail.com>
2025-06-03 01:38:32 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-// import App from './App.tsx'
-import NavBar from './layouts/NavBar'
+import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NavBar />
+    <App />
   </StrictMode>,
 )
